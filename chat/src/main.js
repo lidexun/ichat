2db40946e56e3513e19d960384dd665839f48e91
lidexun <910547462@qq.com>
2022-10-08 18:05:45 +0800

    init

--- chat/src/main.js
+++ chat/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/app.css'
 import App from './App.vue'
+import router from './router/index'
+import { Toast } from 'vant'
+import 'vant/es/toast/style'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Toast)
+app.use(router)
+app.mount('#app')
