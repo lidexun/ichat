8f7e46def208ef1e9e6b079fe3bb6d92cc80ef16
lidexun <910547462@qq.com>
2022-10-08 19:00:49 +0800

    init

--- server/index.js
+++ server/index.js
@@ -1,40 +1,81 @@
-const express = require('express')
-const cors = require('cors')
-
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import morganBody from 'morgan-body'
+import router from './router/index.js'
+import mongoose from 'mongoose'
+// // const ws = require('socket.io')
 const app = express()
-const ws = require('socket.io')
-require('dotenv').config()
-
-app.use(cors())
-app.use(express.json())
-
-const server = app.listen(process.env.PORT, () => {
-  console.log(`Server Stared on Port ${process.env.PORT}`)
-})
-
-const io = ws(server, {
-  cors: {
-    credentials: true
-  }
-})
-
-global.onlineUsers = new Map()
-
-io.on('connection', (socket) => {
-  socket.on('hello', (arg) => {
-    console.log(arg) // world
+dotenv.config()
+// middleware
+// morganBody(app)
+app.use(
+  express.urlencoded({
+    extended: false
   })
-  // console.log(socket)
-  // global.chatSocket = socket
-  // socket.on('add-user', (userId) => {
-  //   console.log(userId)
-  //   onlineUsers.set(userId, socket.id)
-  // })
-  // socket.on('send-msg', (data) => {
-  //   console.log(data)
-  //   const sendUserSocket = onlineUsers.get(data.to)
-  //   if (sendUserSocket) {
-  //     socket.to(sendUserSocket).emit('msg-recieve', data.msg)
-  //   }
-  // })
+)
+app.use(express.json())
+app.use(cors())
+app.use(router)
+// middleware end
+mongoose.connect(
+  process.env.MONGODB_LOCAL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) return console.log(err)
+    console.log('数据库成功连接')
+  }
+)
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server Stared on Port http://localhost:${process.env.PORT}`)
 })
+
+// const chatSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: [true, 'a user must have a name'],
+//     unique: true
+//   },
+//   age: Number
+// })
+// const chat = mongoose.model('Chat', chatSchema)
+
+// chat.find(
+//   {
+//     name: '李'
+//   },
+//   (err, doc) => {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(doc)
+//     }
+//   }
+// )
+
+// const io = ws(server, {
+//   cors: {
+//     credentials: true
+//   }
+// })
+
+// global.onlineUsers = new Map()
+
+// io.on('connection', (socket) => {
+//   socket.on('hello', (arg) => {
+//     console.log(arg) // world
+//   })
+//   console.log(socket)
+//   global.chatSocket = socket
+//   socket.on('add-user', (userId) => {
+//     console.log(userId)
+//     onlineUsers.set(userId, socket.id)
+//   })
+//   socket.on('send-msg', (data) => {
+//     console.log(data)
+//     const sendUserSocket = onlineUsers.get(data.to)
+//     if (sendUserSocket) {
+//       socket.to(sendUserSocket).emit('msg-recieve', data.msg)
+//     }
+//   })
+// })
