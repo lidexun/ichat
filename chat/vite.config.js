075789923875e184d84e407ff6271a7d1a0dfe1e
lidexun <910547462@qq.com>
2022-10-08 17:58:55 +0800

    update: vite.config

--- chat/vite.config.js
+++ chat/vite.config.js
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
+const config = {
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [VantResolver()]
+    }),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router', 'pinia']
+    }),
+    Pages({
+      importMode: 'async',
+      routeStyle: 'nuxt',
+      exclude: ['**/components/*.vue'],
+      dirs: 'src/pages'
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 6868,
+    open: true,
+    https: false,
+    proxy: {}
+  }
+}
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  return {
+    ...config
+  }
+  // if (command === 'serve') {
+  //   return {
+  //     // dev 独有配置
+  //   }
+  // } else {
+  //   // command === 'build'
+  //   return {
+  //     // build 独有配置
+  //   }
+  // }
 })
