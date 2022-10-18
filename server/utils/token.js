dafa79cb9c32f543292f0f0acc82b1ce06c1409a
lidexun <910547462@qq.com>
2022-10-18 23:51:52 +0800

    token封装

--- server/utils/token.js
+++ server/utils/token.js
@@ -1,4 +1,8 @@
 import jwt from 'jsonwebtoken'
-export function generateToken(uid, secret = 'ldx96', expiresIn = '7d') {
-  return jwt.sign({ uid }, secret, { expiresIn })
+export function generateToken(uid, secret, expiresIn = '7d') {
+  return jwt.sign({ uid }, secret || process.env.JWT_SECRET, { expiresIn })
+}
+
+export function verifyToken(token, secret) {
+  return jwt.verify(token, secret || process.env.JWT_SECRET)
 }
