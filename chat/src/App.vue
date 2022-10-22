8825e445b6bfc3b3821b47c51989251e609f8b9f
lidexun <910547462@qq.com>
2022-10-22 15:09:46 +0800

    入口建立ws

--- chat/src/App.vue
+++ chat/src/App.vue
@@ -1,4 +1,35 @@
-<script setup></script>
+<script setup>
+import { io } from 'socket.io-client'
+import emitter from '@/utils/bus.js'
+import storage from '@/utils/storage.js'
+let userInfo = storage.getItem('userInfo')
+let socket = null
+// 登陆后连接ws
+emitter.on('login', () => {
+  init()
+})
+if (userInfo && socket === null) {
+  init()
+}
+function init() {
+  userInfo = storage.getItem('userInfo')
+  socket = io(import.meta.env.VITE_WS_URL, {
+    auth: {
+      username: userInfo.username,
+      id: userInfo._id,
+      token: userInfo.token
+    }
+  })
+  socket.on('message', (data) => {
+    emitter.emit('message', data)
+  })
+}
+// 断开ws
+emitter.on('login_out', (data) => {
+  socket.close()
+  socket = null
+})
+</script>
 
 <template>
   <router-view v-slot="{ Component, route }">
