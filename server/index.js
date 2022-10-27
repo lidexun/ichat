30f355a955c5b7744774045abea1f0eeabd0e7af
lidexun <910547462@qq.com>
2022-10-27 23:17:20 +0800

    token拦截

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import morganBody from 'morgan-body'
-import router from './router/index.js'
 import mongoose from 'mongoose'
+import router from './router/index.js'
 import ws from './utils/socket.js'
+import { expressjwt } from 'express-jwt'
 const app = express({})
 dotenv.config()
 dotenv.config({
@@ -13,7 +14,7 @@ dotenv.config({
 
 // middleware
 if (process.env.NODE_ENV === 'development') {
-  // morganBody(app)
+  morganBody(app)
 }
 app.use(
   express.urlencoded({
@@ -22,7 +23,39 @@ app.use(
 )
 app.use(express.json())
 app.use(cors())
+app.use(
+  expressjwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
+    credentialsRequired: true,
+    getToken: function fromHeaderOrQuerystring(req) {
+      var { token } = req.headers
+      if (token) return token
+      return null
+    }
+  }).unless({
+    path: ['/login', '/register'] //不需要校验的路径
+  })
+)
+app.use((err, req, res, next) => {
+  // 这次失败是由token解析失败导致的
+  if (err) {
+    if (err.code === 'credentials_required') {
+      return res.send({
+        staus: 401,
+        message: err.inner.message
+      })
+    }
+    return res.send({
+      status: 500,
+      message: '未知的错误'
+    })
+  }
+
+  next()
+})
 app.use(router)
+
 // middleware end
 mongoose.connect(
   process.env.MONGODB_LOCAL,
